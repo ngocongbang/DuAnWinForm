69f748bdbbd5c0db1e9f89b977852aba1c4518fc
ngocongbang <30998464+ngocongbang@users.noreply.github.com>
2017-08-14 15:43:16 +0700

    Update Form1.cs
    asfasdfs

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -19,7 +19,11 @@ namespace WindowsFormsApplication1
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            MessageBox.Show("Chao cac ban");          
+            MessageBox.Show("Chao cac ban"); 
+              MessageBox.Show("Chao cac ban");        
+                MessageBox.Show("Chao cac ban");        
+                  MessageBox.Show("Chao cac ban");        
+                    MessageBox.Show("Chao cac ban");        
         }
     }
 }
