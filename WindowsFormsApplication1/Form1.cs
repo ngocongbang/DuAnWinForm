1658feb2edbf7f0396278e20aa57110b4fc869ea
bangnc <bangnc@ominext.com>
2017-08-14 16:20:58 +0700

    asdfasdf

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -20,10 +20,11 @@ namespace WindowsFormsApplication1
         private void Form1_Load(object sender, EventArgs e)
         {
 
-
-           MessageBox.Show("Chao cac ban0");
-            MessageBox.Show("Chao cac ban0");
-             MessageBox.Show("Chao cac ban0");
+            MessageBox.Show("Chao cac ban1");
+            MessageBox.Show("Chao cac ban2");
+            MessageBox.Show("Chao cac ban3");
+            MessageBox.Show("Chao cac ban4");
+            MessageBox.Show("Chao cac ban5");
 
         }
     }
